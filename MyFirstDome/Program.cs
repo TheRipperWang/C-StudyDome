edea41990067d1c584386ff636d493c7722657b8
TheRipperWang <1047859676@qq.com>
2015-08-25 08:31:48 +0800

    alpha

---
+++ MyFirstDome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstDome
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            //int x = 10;
+            //Console.WriteLine( "你好，世界！" );
+            //Console.WriteLine( "Hello World!" );
+            //Console.WriteLine( x );
+            //Console.WriteLine( "王成" );
+
+            Program.Play( );
+
+            Console.WriteLine( "按任意键退出..." );
+            Console.ReadKey( );
+        }
+
+        public static void Play( )
+        {
+            Console.WriteLine( "玩电脑!");
+            Console.WriteLine( "(￣▽￣)\"");
+        }
+    }
+}
---
+++ MySecondDome/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondDome
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine( "信息登记系统" );
+
+            Console.WriteLine( "按任意键退出..." );
+            Console.ReadKey( );
+        }
+    }
+}
